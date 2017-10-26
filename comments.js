b8abdef8ec6c3524d5d664d35f973f1886b3391e
Maciek <m.p.dutkiewicz@gmail.com>
2017-10-26 22:36:34 +0200

    add func

--- comments.js
+++ comments.js
@@ -7,18 +7,41 @@ import {THUMB_DOWN_COMMENT} from './actions';
 function comments(state = [], action) {
     switch (action.type) {
         case ADD_COMMENT:
-            return [{
+        return [
+            {
                 id: action.id,
-                text: action.text
+                text: action.text,
                 votes: 0
-            }
-                ,...state.comments
+            },
+            ...state
         ];
         case REMOVE_COMMENT:
-            return [{
-                state.comments.filter(comment => comment.id !== action.id)
-            }];
+            return state.filter(comment => comment.id !== action.id);
+        case THUMB_UP_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {...comment, votes: comment.votes + 1}
+                }
+
+                return comment;
+            });
+        case THUMB_DOWN_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {...comment, votes: comment.votes - 1}
+                }
+
+                return comment;
+            });
+        case EDIT_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {...comment, text: action.text}
+                }
+
+                return comment;
+            });
         default:
-            return: state;
+            return state;
     }
 }
